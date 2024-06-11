3fabf24e75dbec299a44197e304db000750bf2c1
nagarjunacse02 <32007728+nagarjunacse02@users.noreply.github.com>
2024-06-11 13:41:00 +0530

    Create svc_check_with_condition.sh

---
+++ nagarjunacse02/svc_check_with_condition.sh
@@ -0,0 +1,26 @@
+response=$(curl -u Svc-RetirementDevops:<+secrets.getValue("org.rngartifactorysecret")> -s -X GET https://artifactory.fis.dev/artifactory/rcs-helm-release-local/)
+echo $response
+files=$(echo "$response"| grep -oP '(?<=href=")[^"]+\.tgz')
+echo "Files found:"
+echo "$files"
+
+filter_files=$(echo "$files" | grep '^core-ivr-service')
+echo "core-ivr-service file found:$filter_files"
+
+version_files=$(echo "$filter_files" | grep "core-ivr-service-<+pipeline.variables.Major_version>\.")
+if [ -z "$version_files" ]; then
+   echo "Error: No files found with core-ivr-service svc version"
+   exit 1
+fi   
+
+latest_file=$(echo "$version_files" | sort -V | tail -n 1)
+echo "Sorted result:$latest_file"
+
+file_base_name=$(basename "$latest_file" .tgz)
+#file_base_name=$(basename "$file_base_name" -SNAPSHOT)
+
+echo "base name result:$file_base_name"
+
+version=$(echo "$file_base_name" | grep -oP '\d+\.\d+\.\d+')
+echo "Final major version result:$version"
+echo "$version"
